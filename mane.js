04062a6fcb2a43f94f4218c01c70b77d501be701
f3dg3r <150592689+f3dg3r@users.noreply.github.com>
2023-12-02 14:00:52 +1000

    Add files via upload

---
+++ mane.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('open');
+  })
+
+  document.querySelector('.menu-close').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('open');
+  })
+
+
